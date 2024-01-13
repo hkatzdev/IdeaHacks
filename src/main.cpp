4cb6962480c36a127343e6254bbf82629befa4b1
CurtisManacsa <111934214+CurtisManacsa@users.noreply.github.com>
2024-01-12 23:45:50 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
-#include <Arduino.h>
+const int HYGROMIN = 400;
+const int HYGROMAX = 1023;
+const int UPPER = 100;
+const int LOWER = 0;
 
-// put function declarations here:
-int myFunction(int, int);
+const int hygrometer_1 = A0;
+const int hygrometer_2 = A1;
+const int hygrometer_3 = A2;
+int hygrometer_val;
+
+int moisturePercent(int value){
+  return map(constrain(value, HYGROMIN, HYGROMAX), HYGROMIN, HYGROMAX, UPPER, LOWER);
+}
+
+int printHydrogmeter(int hygrometer){
+    hygrometer_val = moisturePercent(analogRead(hygrometer));
+    switch(hygrometer){
+      case A0:
+        Serial.print("Lvl 1 Moisture:");
+        break;
+      case A1:
+        Serial.print("Lvl 2 Moisture:");
+        break;
+      case A2:
+        Serial.print("Lvl 3 Moisture:");
+        break;
+  }
+  Serial.print(hygrometer_val);
+  Serial.print("%\n");
+  delay(1000);
+}
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
+  printHydrogmeter(hygrometer_1);
+  printHydrogmeter(hygrometer_2);
+  printHydrogmeter(hygrometer_3);
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
 }
